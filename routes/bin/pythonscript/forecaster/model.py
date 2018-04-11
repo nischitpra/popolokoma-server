c7ba39a21fdf86ea61baf6e41646ddcc679a72a2
nischitpra <nischitpra@gmail.com>
2018-04-11 15:24:26 +0530

    small changes to mongo path

--- routes/bin/pythonscript/forecaster/model.py
+++ routes/bin/pythonscript/forecaster/model.py
@@ -12,7 +12,7 @@ from pymongo import MongoClient
 client = MongoClient()
 client = MongoClient('mongodb://heroku_w06gvgdc:39i4hl2t7g5fqejfb07jbb9gf4@ds241059.mlab.com:41059/heroku_w06gvgdc')
 db_name = 'heroku_w06gvgdc'
-db = client.[db_name]
+db = client[db_name]
 
 
 
--- routes/bin/pythonscript/good_bad_tweet.py
+++ routes/bin/pythonscript/good_bad_tweet.py
@@ -14,7 +14,7 @@ from bson.objectid import ObjectId
 client = MongoClient()
 client = MongoClient('mongodb://heroku_w06gvgdc:39i4hl2t7g5fqejfb07jbb9gf4@ds241059.mlab.com:41059/heroku_w06gvgdc')
 db_name = 'heroku_w06gvgdc'
-db = client.[db_name]
+db = client[db_name]
 
 base_path='/app/routes/bin/pythonscript'
 HISTORY_TYPE=1*60*60*24 #1 day
